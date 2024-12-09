e71255113123ef8559d4666d8846ef1ae77a58f2
Isabella Reis <isabella.dreis@mercadolivre.com>
2024-12-09 11:14:11 -0300

    Initial commit

---
+++ src/test/java/com/helpdesk/helpdeskApi/HelpdeskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helpdesk.helpdeskApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
